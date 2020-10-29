75507696bc5bb6f381d404b84a8d494db8504d5f
Ted Bradley <ted.bradley@hudl.com>
2020-10-29 13:33:09 +0000

    Initial Commit

---
+++ MetalVideoDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MetalVideoDemo
+//
+//  Created by Ted Bradley on 29/10/2020.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MetalVideoDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MetalVideoDemo
+//
+//  Created by Ted Bradley on 29/10/2020.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
